b4ca39273e03ac74c45d9d0a5c5d4f8280e31e68
zyihong <zyihong@umich.edu>
2020-12-18 20:58:29 -0500

    utils.js - register

--- jupiter-frontend/src/utils.js
+++ jupiter-frontend/src/utils.js
@@ -17,3 +17,18 @@ export const login = (credential) => {
         return response.json();
     })
 }
+
+const registerUrl = `${SERVER_ORIGIN}/register`;
+export const register = (data) => {
+    return fetch(registerUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }).then((response) => {
+        if (response.status !== 200) {
+            throw Error('Fail to register!');
+        }
+    })
+}
