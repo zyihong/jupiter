5f90324be25c1750de56043ac6f4efc8125954c0
zyihong <zyihong@umich.edu>
2020-12-23 20:15:31 -0500

    recommend by favorite games

--- src/main/java/com/example/jupiter/recommendation/ItemRecommender.java
+++ src/main/java/com/example/jupiter/recommendation/ItemRecommender.java
@@ -7,7 +7,10 @@ import com.example.jupiter.twitch.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 public class ItemRecommender {
@@ -40,10 +43,43 @@ public class ItemRecommender {
         return recommendedItems;
     }
 
-    // if user logged in and has favorited some resource, use this API
+    // if user logged in and has favorited some resource, use this API.
     private List<Item> recommendByFavorite(ItemType type, Set<String> favoriteItemIds,
                                            List<String> favoriteGameIds) throws RecommendationException {
-        System.out.println("TODO");
-        return null;
+        List<Item> recommendedItems = new ArrayList<>();
+        TwitchClient client = new TwitchClient();
+
+        // Turn the favorite game id to a count map and sort by count ao descending.
+        Map<String, Long> favoriteGameIdByCount = favoriteGameIds.parallelStream()
+                .collect(Collectors.groupingBy(str -> str, Collectors.counting()));
+
+        List<Map.Entry<String, Long>> sortedFavoriteGameIdByCount = new ArrayList<>(favoriteGameIdByCount.entrySet());
+        sortedFavoriteGameIdByCount.sort(
+                (e1, e2) -> Long.compare(e2.getValue(), e1.getValue())
+        );
+
+        // Truncate the games.
+        if (sortedFavoriteGameIdByCount.size() > DEFAULT_GAME_LIMIT) {
+            sortedFavoriteGameIdByCount = sortedFavoriteGameIdByCount.subList(0, DEFAULT_GAME_LIMIT);
+        }
+
+        for (Map.Entry<String, Long> gameEntry : sortedFavoriteGameIdByCount) {
+            List<Item> items;
+            try {
+                items = client.searchByTypes(gameEntry.getKey(), type, DEFAULT_PER_GAME_RECOMMENDATION_LIMIT);
+            } catch (TwitchException e) {
+                throw new RecommendationException("Fail to get favorite game recommendations!");
+            }
+
+            for (Item item : items) {
+                if (recommendedItems.size() >= DEFAULT_TOTAL_RECOMMENDATION_LIMIT) break;
+
+                // Avoid recommending item that is already favorited.
+                if (!favoriteItemIds.contains(item.getId())) recommendedItems.add(item);
+            }
+
+            if (recommendedItems.size() >= DEFAULT_TOTAL_RECOMMENDATION_LIMIT) break;
+        }
+        return recommendedItems;
     }
 }
